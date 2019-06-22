3a274ba08694ba4bae18cc3e062b7253a68b7312
pireddag <48107401+pireddag@users.noreply.github.com>
2019-06-22 11:50:23 +0200

    Style file
    In ~/.TeXmacs/packages/formatRef/Formatted references.ts

---
+++ Formatted references.ts
@@ -0,0 +1,27 @@
+<TeXmacs|1.99.9>
+
+<style|source>
+
+<\body>
+  <use-module|(formatRef interfaceToTeXmacs)>
+
+  <assign|formatRef|<macro|str|<extern|formatRefScheme|<arg|str>><reference|<arg|str>>>>
+
+  <assign|addToFormatRef|<macro|formatCode|formatString|<extern|addToFormatListScheme|<arg|formatCode>|<arg|formatString>>>>
+
+  <assign|deleteFromFormatRef|<macro|formatCode|n|<extern|deleteFromFormatListScheme|<arg|formatCode>|<arg|n>>>>
+
+  <assign|replaceInFormatRef|<macro|formatCode|formatString|n|<extern|replaceInFormatListScheme|<arg|formatCode>|<arg|formatString>|<arg|n>>>>
+
+  \;
+
+  \;
+
+  \;
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
