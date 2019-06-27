7e5f1071869e0a18750b365285004b7e440874f7
pireddag <48107401+pireddag@users.noreply.github.com>
2019-06-27 12:02:28 +0200

    With no match, return reference
    The macro formatRef checks the length of the string returned by formatRefScheme. If it is equal to zero, it returns <reference|lab>.

--- Formatted references.ts
+++ Formatted references.ts
@@ -5,7 +5,7 @@
 <\body>
   <use-module|(formatRef interfaceToTeXmacs)>
 
-  <assign|formatRef|<macro|str|<extern|formatRefScheme|<arg|str>>>>
+  <style-with|src-compact|none|<assign|formatRef|<style-with|src-compact|none|<macro|str|<style-with|src-compact|none|<with|fmtRef|<extern|formatRefScheme|<arg|str>>|<style-with|src-compact|none|<case|<equal|<length|<value|fmtRef>>|0>|<reference|<arg|str>>|<value|fmtRef>>>>>>>>>
 
   <assign|addToFormatRef|<macro|formatCode|formatString|<extern|addToFormatListScheme|<arg|formatCode>|<arg|formatString>>>>
 
