26a0f4e3635a7a6ffb8c0006daa70efb7f73129d
pireddag <48107401+pireddag@users.noreply.github.com>
2019-10-08 22:48:52 +0200

    Added tests; reorganizing to test with mutability
    Added tests; started reorganizing to make testing work nicely with state mutability; notes to rewrite function with set-rest formatList so that I do not have to keep track of it

--- Test_Scheme/testFormatRef.scm
+++ Test_Scheme/testFormatRef.scm
@@ -1,3 +1,9 @@
+;; but I need to find a better and general solution so that I test without worrying about the state of formatList
+;; (perhaps a function that sets and resets formatList, and I also would like it not to print anything to screen,
+;; as set! does and I do not want to see this output in this "unit testing")
+;; try this https://stackoverflow.com/questions/50693165/is-there-a-way-to-turn-down-the-verbosity-of-mit-scheme
+;; tried - it works
+
 (load "/home/giovanni/test/test TeXmacs/De Benito/version_04/selectFormat.scm")
 (load "/home/giovanni/test/test TeXmacs/De Benito/version_04/formatRefUtils.scm")
 (load "/home/giovanni/test/test TeXmacs/De Benito/version_04/manipulateList.scm")
@@ -10,19 +16,19 @@
 ;; the quotes and quasiquotes define a list without using the "list" command
 
 ;; Checks that a single format formatGroup is a list of two elements.
-(display "testing checkGroup")
+(display "testing checkGroup\n")
 (test-expression "not a list" (checkGroup "eq") #f)
 (test-expression "a list of one element" (checkGroup '("eq1")) #f)
 (test-expression "a list of two elements" (checkGroup '("eq1" "eq2")) #t)
 (test-expression "a list of three elements" (checkGroup '("eq1" "eq2" "eq3")) #f)
 
 ;; Checks that all elements of formatGroup are strings.
-(display "testing checkFormatStrings")
+(display "testing checkFormatStrings\n")
 (test-expression "a list of three elements, all of them strings" (checkFormatStrings '("eq1" "eq2" "eq3")) #t)
 (test-expression "a list of three elements, one of them is not a string" (checkFormatStrings '("eq1" 2 "eq3")) #f)
 ;; checkFormatStrings can only be applied to lists (attempt to apply to a non-list results in error)
 
-(display "testing checkFormatGroup")
+(display "testing checkFormatGroup\n")
 (test-expression "a list of two elements, both strings" (checkFormatGroup '("eq:" "equation")) #t)
 (test-expression "a list of three elements" (checkFormatGroup '("eq:" "equation" "eqn.")) #f)
 (test-expression "not a list" (checkFormatGroup "eq:") #f)
@@ -31,7 +37,7 @@
 
 ;; Compare two formats (different from the “matching” function because it compares the lists that define formats)
 ;; Compares the cars of two formats, taking for granted that both are synctactically correct
-(display "testing checkFormatEqual")
+(display "testing checkFormatEqual\n")
 (test-expression "car is the same" (checkFormatEqual '("eq:" "equation") '("eq:" "Equation")) #t)
 (test-expression "car is different" (checkFormatEqual '("eq:" "equation") '("fig:" "Equation")) #f)
 
@@ -39,6 +45,32 @@
 ;; Takes for granted:
 ;; formatList exists and is correctly formed
 ;; the input formatGroup is correctly formed
-(display "testing checkFormatPresent")
+(display "testing checkFormatPresent\n")
 (test-expression "format is not there" (checkFormatPresent '("sec:" "section")) #f)
 (test-expression "format is there" (checkFormatPresent '("fig:" "figure")) #t)
+
+;; Check that a format can be added to the lists fo formats
+;; I would like to check this function making sure that the format group goes through the two different checks
+;; (correct syntax, and format not present) rather than checking if format is correct
+;; I do not yet know how to check in this way
+(display "testing checkFormatGroupAdd\n")
+
+;; add the format to the list
+;; here too I would like to check that the check takes place, and I need to figure out a way for this.
+;; by the way this shows that checking a function with mutable data is more difficult than checking a function which does not mutate data
+;; in fact now
+;; 1) the check is not on the output of the function
+;; 2) I have to take into account that after the application of the function formatList is different
+;; I "save" myself by resetting formatList, but I need to find a better and general solution so that I test without worrying about the state of formatList
+;; (perhaps a function that sets and resets formatList, and I also would like it not to print anything to screen,
+;; as set! does and I do not want to see this output in this "unit testing")
+;; try this https://stackoverflow.com/questions/50693165/is-there-a-way-to-turn-down-the-verbosity-of-mit-scheme
+(display "testing addToFormatList\n")
+(begin
+  (addToFormatList '("sec:" "Section"))
+  (test-expression "adding a format to the list (here we check the resulting format list with one that we have prepared)" (equal?
+						  formatList
+						  (list (list "eq:" "Equation") (list "fig:" "Figure") (list "tab:" "Table") (list "sec:" "Section")))
+		   #t)
+  (set! formatList (list (list "eq:" "Equation") (list "fig:" "Figure") (list "tab:" "Table"))))
+
